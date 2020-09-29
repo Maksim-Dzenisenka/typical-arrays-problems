558114fd798bcafe957f87bceaaa5f96528acee9
Maksim_Dzenisenka <maksdenisenko@gmail.com>
2020-09-29 21:32:48 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let min = Math.min.apply(null, array);
+    return min;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let max = Math.max.apply(null, array);
+    return max;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let avg = array.reduce((a, b) => (a + b)) / array.length;
+    return avg;
+  }
 }
